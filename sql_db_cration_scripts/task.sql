5b2ebe934e785fe4921a1f3d954f4a0ff4bf4f05
Diana Ivanova <didi.e.ivanova@gmail.com>
2020-02-06 12:55:14 +0200

    added tags column and actual_hours column

--- sql_db_cration_scripts/task.sql
+++ sql_db_cration_scripts/task.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2020 at 10:46 PM
+-- Generation Time: Feb 06, 2020 at 11:53 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.10
 
@@ -13,6 +13,21 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `estimateme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
 CREATE TABLE `task` (
   `title` varchar(200) NOT NULL,
   `description` varchar(500) DEFAULT NULL,
@@ -20,11 +35,25 @@ CREATE TABLE `task` (
   `creation_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` enum('in_progress','finished') NOT NULL,
   `finish_date` timestamp NULL DEFAULT NULL,
-  `expert_estimation` int(11) NOT NULL
+  `expert_estimation` int(11) NOT NULL,
+  `tags` varchar(255) NOT NULL,
+  `actual_hours` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `task`
+--
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `task`
+--
 ALTER TABLE `task`
   ADD PRIMARY KEY (`title`,`project_name`);
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
