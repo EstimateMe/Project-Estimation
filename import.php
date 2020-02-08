72cc23876c64545be86bdb0a617becb7fa49bc69
Yana Zdravkova <yana.zdravkovaa@gmail.com>
2020-02-08 18:21:46 +0200

    Add import of multiple projects

--- import.php
+++ import.php
@@ -1,10 +1,10 @@
 <?php
 // Start the session
 session_start();
-   require_once('dbConnect.php');
-   $user = $_SESSION['session_user'];
-  $sql = 'SELECT * FROM `task` where project_name=?';
-  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+require_once('dbConnect.php');
+$user = $_SESSION['session_user'];
+$sql  = 'SELECT * FROM `task` where project_name=?';
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 ?>
 
 <html>
@@ -12,19 +12,28 @@ session_start();
 </head>
 <body>
 <?php
-    //read the json file contents
-    $jsondata =file_get_contents('test.json');
-    $data = json_decode($jsondata);
-    $name = $data->name;
-    $created_at = $data->created_at; // TODO: handle
-    $expert_estimation = $data->expert_estimation;
-    $sql=$conn->prepare("INSERT INTO `project`(`name`, `created_at`, `expert_estimation`) 
-	      VALUES (:name,:created_at,:expert_estimation)");
-      $sql->execute(['name'=>$name,'created_at'=>$created_at,'expert_estimation'=>$expert_estimation]);
-    $sql=$conn->prepare("INSERT INTO `project-user`(`username`, `projectName`, `isOwner`) 
+//read the json file contents
+$jsondata = file_get_contents('test.json');
+$data     = json_decode($jsondata);
+
+foreach ($data as $projectJson) {
+    $name              = $projectJson->name;
+    $created_at        = $projectJson->created_at; // TODO: handle
+    $expert_estimation = $projectJson->expert_estimation;
+    
+    $sql = $conn->prepare("INSERT INTO `project`(`name`, `created_at`, `expert_estimation`) 
+          VALUES (:name,:created_at,:expert_estimation)");
+    $sql->execute(['name'=>$name,'created_at'=>$created_at,'expert_estimation'=>$expert_estimation]);
+
+    $sql = $conn->prepare("INSERT INTO `project-user`(`username`, `projectName`, `isOwner`) 
       VALUES (:username,:projectName,:isOwner)");
     $sql->execute(['username'=>$user,'projectName'=>$name,'isOwner'=>true]);
-      echo "\nDATA SUCCESSFULY IMPORTED!";
+
+    echo "\nDATA SUCCESSFULY IMPORTED!";
+    
+}
 ?>
 </body>
 </html>
+
+
