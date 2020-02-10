7c670428c8b2868036cc19714a0b982430c68d5a
Yana Zdravkova <yana.zdravkovaa@gmail.com>
2020-02-10 19:29:27 +0200

    Fix tags sql

--- sql_db_cration_scripts/tags.sql
+++ sql_db_cration_scripts/tags.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS `tags` (
   `hours` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `tag`
+ALTER TABLE `tags`
   ADD PRIMARY KEY (`tag`);
   
-INSERT INTO `tag`(`tag`, `hours`) VALUES ('html',0),('sql',0),('javascript',0),('css',0),('php',0),
+INSERT INTO `tags`(`tag`, `hours`) VALUES ('html',0),('sql',0),('javascript',0),('css',0),('php',0),
 ('page',2),('form',2),('button',1),('hyperlink',1),('picture',1), ('database',2),('table',2),('column',2),
 ('validation',2),('ajax request',2),('event',1),('insert into db',1), ('connect to db',1), ('query from db',2);
